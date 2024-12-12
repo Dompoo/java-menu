18a995ec7f9ba8eecc9099ddcb09fdfd76ad95a9
이창근 <dlckdrms0517@gmail.com>
2024-12-13 02:25:18 +0900

    feat: 컨트롤러의 코드를 재시도하도록 코드조각으로 분리

--- src/main/java/menu/controller/Controller.java
+++ src/main/java/menu/controller/Controller.java
@@ -1,13 +1,13 @@
 package menu.controller;
 
 import menu.common.dto.MenuPickResults;
+import menu.common.retry.RetryHandler;
 import menu.domain.LaunchMate;
 import menu.domain.LaunchMates;
 import menu.domain.Menu;
 import menu.io.input.InputHandler;
 import menu.io.output.OutputHandler;
 import menu.service.numberPicker.NumberPicker;
-import menu.service.numberPicker.RandomNumberPicker;
 import menu.service.objectPicker.ObjectPicker;
 
 import java.util.ArrayList;
@@ -15,12 +15,14 @@ import java.util.List;
 
 public class Controller {
 	
+	private final RetryHandler retryHandler;
 	private final InputHandler inputHandler;
 	private final OutputHandler outputHandler;
 	private final NumberPicker numberPicker;
 	private final ObjectPicker<Menu> menuPicker;
 	
-	public Controller(InputHandler inputHandler, OutputHandler outputHandler, RandomNumberPicker numberPicker, ObjectPicker<Menu> menuPicker) {
+	public Controller(RetryHandler retryHandler, InputHandler inputHandler, OutputHandler outputHandler, NumberPicker numberPicker, ObjectPicker<Menu> menuPicker) {
+		this.retryHandler = retryHandler;
 		this.inputHandler = inputHandler;
 		this.outputHandler = outputHandler;
 		this.numberPicker = numberPicker;
@@ -29,13 +31,21 @@ public class Controller {
 	
 	public void run() {
 		List<LaunchMate> mates = new ArrayList<>();
-		List<String> names = inputHandler.handleLaunchMateNames();
+		
+		List<String> names = retryHandler.tryUntilSuccess(inputHandler::handleLaunchMateNames);
+		
 		for (String name : names) {
-			List<String> noEatMenuNames = inputHandler.handleNoEatMenuNames(name);
-			mates.add(LaunchMate.of(name, noEatMenuNames));
+			retryHandler.tryUntilSuccess(() -> {
+				List<String> noEatMenuNames = inputHandler.handleNoEatMenuNames(name);
+				return mates.add(LaunchMate.of(name, noEatMenuNames));
+			});
 		}
-		LaunchMates launchMates = new LaunchMates(mates);
-		MenuPickResults result = launchMates.pickMenus(numberPicker, menuPicker);
+		
+		MenuPickResults result = retryHandler.tryUntilSuccess(() -> {
+			LaunchMates launchMates = new LaunchMates(mates);
+			return launchMates.pickMenus(numberPicker, menuPicker);
+		});
+		
 		outputHandler.handleMenuPickResults(result);
 	}
 }

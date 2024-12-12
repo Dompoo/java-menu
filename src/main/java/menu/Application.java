b56a6f95d35a5cb95de8fb79cc4ec95d37896bc1
이창근 <dlckdrms0517@gmail.com>
2024-12-13 02:27:12 +0900

    feat: Application 작성

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,28 @@
 package menu;
 
+import menu.common.exceptin.ExceptionHandler;
+import menu.common.retry.RetryHandler;
+import menu.controller.Controller;
+import menu.io.input.InputHandler;
+import menu.io.input.InputParser;
+import menu.io.input.InputValidator;
+import menu.io.output.OutputHandler;
+import menu.io.output.OutputParser;
+import menu.io.reader.ConsoleReader;
+import menu.io.writer.ConsoleWriter;
+import menu.io.writer.Writer;
+import menu.service.numberPicker.RandomNumberPicker;
+import menu.service.objectPicker.RandomObjectPicker;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Writer writer = new ConsoleWriter();
+        new Controller(
+                new RetryHandler(new ExceptionHandler(writer)),
+                new InputHandler(new ConsoleReader(), writer, new InputValidator(), new InputParser()),
+                new OutputHandler(writer, new OutputParser()),
+                new RandomNumberPicker(),
+                new RandomObjectPicker<>()
+        ).run();
     }
 }
